355fbbdaacd9f94f14c279ab1107e37aa07aefe5
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-11-20 21:35:49 +0000

    Implement the basic functionality of the UI

--- src/main/java/armadillo/View.java
+++ src/main/java/armadillo/View.java
@@ -1,4 +1,4 @@
-package main.java.armadillo;
+package armadillo;
 
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -8,23 +8,122 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class View extends Application{
 	
 	public static void main(String[] args) {
         launch(args);
     }
+
+
+	static class checkBoxCell extends ListCell<String> {
+
+		public void updateItem(String item, boolean empty) {
+			super.updateItem(item, empty);
+			if (empty) {
+				setText(null);
+				setGraphic(null);
+			} else {
+				setText(item);
+
+				CheckBox checkbox = new CheckBox();
+				setGraphic(checkbox);
+			}
+		}
+	}
+
+	static class taskCell extends ListCell<String> {
+
+		HBox hbox = new HBox();
+
+
+		CheckBox checkbox = new CheckBox();
+		Button button = new Button("Delete");
+		Label label = new Label("");
+
+		public taskCell(){
+			super();
+			hbox.setSpacing(5);
+			hbox.getChildren().addAll(checkbox, label, button);
+		}
+
+		@Override
+		public void updateItem(String item, boolean empty) {
+			super.updateItem(item, empty);
+			if (empty) {
+				setText(null);
+				setGraphic(null);
+			} else {
+				label.setText(item);
+				setGraphic(hbox);
+			}
+		}
+	}
+
+	static class resourcesCell extends ListCell<String> {
+		HBox hbox = new HBox();
+		Button button = new Button("Delete");
+		Label label = new Label("");
+
+		public resourcesCell(){
+			super();
+			hbox.setSpacing(5);
+			hbox.getChildren().addAll(label, button);
+		}
+
+		@Override
+		public void updateItem(String item, boolean empty) {
+			super.updateItem(item, empty);
+			if (empty) {
+				setText(null);
+				setGraphic(null);
+			} else {
+				label.setText(item);
+				setGraphic(hbox);
+			}
+		}
+
+	}
+
+	static class peopleCell extends ListCell<String> {
+		HBox hbox = new HBox();
+		Pane pane = new Pane();
+		Button button = new Button("Delete");
+		Label label = new Label("");
+
+		public peopleCell(){
+			super();
+			hbox.setSpacing(5);
+			hbox.getChildren().addAll(label, pane, button);
+			hbox.setHgrow(pane, Priority.ALWAYS);
+
+		}
+
+		@Override
+		public void updateItem(String item, boolean empty) {
+			super.updateItem(item, empty);
+			if (empty) {
+				setText(null);
+				setGraphic(null);
+			} else {
+				label.setText(item);
+				setGraphic(hbox);
+			}
+		}
+
+	}
 
 	@Override
     public void start(Stage primaryStage) throws Exception{
@@ -53,13 +152,24 @@ public class View extends Application{
 
 		Button resource = new Button("Add Resources");
 		grid.add(resource, 2, 1);
-		
-		
-		
+
+
+
+
+
 		// ------------- Change to MVC ---------------
 		task.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
+
+				Button submit = new Button("Submit");
+
+				Label personInstructions = new Label("Select people to assign to task.");
+				Label taskInstructions = new Label("Select prerequisite tasks or delete pre-existing tasks.");
+				Label resourcesInstructions = new Label("Enter required resources.");
+
+				Label blankLabel = new Label();
+				TextField resourcesField = new TextField();
 				
 				Label taskLabel = new Label("Task Name: ");
 		        // taskLabel.setAlignment(Pos.TOP_LEFT);
@@ -68,28 +178,74 @@ public class View extends Application{
 		        //taskField.setAlignment(Pos.TOP_CENTER);
 		        taskField.setMaxSize(200,25);
 
-		        Label effortLabel = new Label("Effort Estimate: ");
+		        Label taskDescription = new Label("Task Description: ");
 
-		        TextField effortField = new TextField();
-		        effortField.setMaxSize(200,25);
+		        TextField descriptionField = new TextField();
+				descriptionField.setMaxSize(200,100);
 
-		        ListView<String> listOfTasks = new ListView<String>();
+
+
+				ObservableList<String> people = FXCollections.observableArrayList (
+						"Person #1", "Person #2");
+				ListView<String> listOfPeople = new ListView<>(people);
+				listOfPeople.setCellFactory(param -> new checkBoxCell());
+
+
 		        ObservableList<String> items = FXCollections.observableArrayList (
 		                "Task #1", "Task #2", "Task #3");
-		        listOfTasks.setItems(items);
+				ListView<String> listOfTasks = new ListView<String>(items);
+		        listOfTasks.setCellFactory(param -> new taskCell());
+
+				ObservableList<String> resources = FXCollections.observableArrayList (
+						"Resource #1", "Resource #2", "Resource #3");
+		        ListView<String> listOfResources = new ListView<String>(resources);
+		        listOfResources.setCellFactory(param -> new resourcesCell());
 
 			
 				Stage dialog = new Stage();
 				dialog.initModality(Modality.APPLICATION_MODAL);
-				VBox dialogVbox = new VBox(20);
-	
-		        dialogVbox.getChildren().add(taskLabel);
-		        dialogVbox.getChildren().add(taskField);
-		        dialogVbox.getChildren().add(effortLabel);
-		        dialogVbox.getChildren().add(effortField);
-		        dialogVbox.getChildren().add(listOfTasks);
+				HBox dialogHbox = new HBox(20);
+				dialogHbox.setPadding(new Insets(25,25,25,25));
+				BorderPane borderPane = new BorderPane();
+
+				VBox peopleVbox = new VBox();
+				peopleVbox.getChildren().add(personInstructions);
+				//peopleVbox.getChildren().add(blankLabel);
+				peopleVbox.setSpacing(25);
+				peopleVbox.getChildren().add(listOfPeople);
+
+				VBox taskVbox = new VBox();
+				taskVbox.getChildren().add(taskInstructions);
+				//taskVbox.getChildren().add(blankLabel);
+				taskVbox.setSpacing(25);
+				taskVbox.setPadding(new Insets(0,10,0,10));
+				taskVbox.getChildren().add(listOfTasks);
+
+				VBox resourcesVbox = new VBox();
+				resourcesVbox.getChildren().add(resourcesInstructions);
+				resourcesVbox.getChildren().add(resourcesField);
+				resourcesVbox.getChildren().add(listOfResources);
+
+				dialogHbox.setAlignment(Pos.CENTER);
+
+		        dialogHbox.getChildren().add(taskLabel);
+		        dialogHbox.getChildren().add(taskField);
+				dialogHbox.getChildren().add(taskDescription);
+		        dialogHbox.getChildren().add(descriptionField);
+
+		        BorderPane buttonPane = new BorderPane();
+		        buttonPane.setRight(submit);
+				borderPane.setBottom(buttonPane);
+
+				borderPane.setLeft(peopleVbox);
+				borderPane.setCenter(taskVbox);
+				borderPane.setRight(resourcesVbox);
+
+				borderPane.setTop(dialogHbox);
+
+				borderPane.setPadding(new Insets(25,25,25,25));
 		        
-		        Scene dialogScene = new Scene(dialogVbox, 500, 600);
+		        Scene dialogScene = new Scene(borderPane, 850, 500);
 		        dialog.setScene(dialogScene);
 		        dialog.show();
 					
@@ -107,20 +263,21 @@ public class View extends Application{
 		        TextField nameField = new TextField();
 		        nameField.setMaxSize(200,25);
 
-		        ListView<String> listOfPeople = new ListView<String>();
-		        ObservableList<String> items = FXCollections.observableArrayList (
+
+		        ObservableList<String> people = FXCollections.observableArrayList (
 		                "Nick", "Eric");
-		        listOfPeople.setItems(items);
+				ListView<String> listOfPeople = new ListView<String>(people);
+				listOfPeople.setCellFactory(param -> new peopleCell());
 
 		        final Stage dialog = new Stage();
 		        dialog.initModality(Modality.APPLICATION_MODAL);
 		        VBox dialogVbox = new VBox(20);
-
+				dialogVbox.setPadding(new Insets(25,25,25,25));
 		        dialogVbox.getChildren().add(nameLabel);
 		        dialogVbox.getChildren().add(nameField);
 		        dialogVbox.getChildren().add(listOfPeople);
 
-		        Scene dialogScene = new Scene(dialogVbox, 500, 600);
+		        Scene dialogScene = new Scene(dialogVbox, 500, 500);
 		        dialog.setScene(dialogScene);
 		        dialog.show();
 			}
@@ -133,18 +290,25 @@ public class View extends Application{
 			
 			@Override
 			public void handle(ActionEvent event) {
-				Label recourseLabel = new Label("Enter Recourse: ");
+				Label recourseLabel = new Label("Enter Resource: ");
 		
 		        TextField recourseField = new TextField();
 		        recourseField.setMaxSize(200,25);
+
+				ObservableList<String> resource = FXCollections.observableArrayList (
+						"Resource #1", "Resource #2");
+				ListView<String> listOfResources = new ListView<String>(resource);
+				listOfResources.setCellFactory(param -> new peopleCell());
 		
 		        final Stage dialog = new Stage();
 		        dialog.initModality(Modality.APPLICATION_MODAL);
 		        VBox dialogVbox = new VBox(20);
+				dialogVbox.setPadding(new Insets(25,25,25,25));
 		        dialogVbox.getChildren().add(recourseLabel);
 		        dialogVbox.getChildren().add(recourseField);
+		        dialogVbox.getChildren().add(listOfResources);
 		
-		        Scene dialogScene = new Scene(dialogVbox, 500, 600);
+		        Scene dialogScene = new Scene(dialogVbox, 500, 500);
 		        dialog.setScene(dialogScene);
 		        dialog.show();
 			}
