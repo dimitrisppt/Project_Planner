8722e778e801841b48319d430defd9969f4d6b25
Robert Greener <robert.greener@kcl.ac.uk>
2017-11-25 00:29:15 +0000

    Issue #19 Database helper class added

---
+++ src/main/java/armadillo/models/Database.java
@@ -0,0 +1,27 @@
+package armadillo.models;
+
+import java.sql.*;
+
+public class Database {
+    public final String URL = "jdbc:sqlite:build/resources/test/test.db";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        return DriverManager.getConnection(url);
+    }
+
+    public static void executeStatement(String sql) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        Statement sqlStatement = conn.createStatement();
+        sqlStatement.execute(sql);
+        conn.close();
+    }
+
+    public static ResultSet executeQuery(String sql) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        Statement sqlStatement = conn.createStatement();
+        ResultSet rs = sqlStatement.executeQuery(sql);
+        conn.close();
+        return rs;
+    }
+}
