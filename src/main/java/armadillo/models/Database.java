6597d6748f00df198ed8bf296ea06caec951f97a
Robert Greener <robert.greener@kcl.ac.uk>
2017-11-25 00:32:48 +0000

    Issue #19 Added executeInsertStatement

--- src/main/java/armadillo/models/Database.java
+++ src/main/java/armadillo/models/Database.java
@@ -17,6 +17,15 @@ public class Database {
         conn.close();
     }
 
+    public static ResultSet executeInsertStatement(String sql) throws SQLException, ClassNotFoundException {
+        Connection conn = getConnection();
+        Statement sqlStatement = conn.createStatement();
+        sqlStatement.execute(sql);
+        ResultSet rs = sqlStatement.getGeneratedKeys();
+        conn.close();
+        return rs;
+    }
+
     public static ResultSet executeQuery(String sql) throws SQLException, ClassNotFoundException {
         Connection conn = getConnection();
         Statement sqlStatement = conn.createStatement();
