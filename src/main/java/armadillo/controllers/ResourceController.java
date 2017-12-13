5844c3a69f926ebd36411643648e5875d29e6bba
Robert Greener <robert.greener@kcl.ac.uk>
2017-12-13 15:48:55 +0000

    Documented ResourceController

--- src/main/java/armadillo/controllers/ResourceController.java
+++ src/main/java/armadillo/controllers/ResourceController.java
@@ -9,16 +9,34 @@ import armadillo.views.ResourcesPanel;
 import java.sql.SQLException;
 import java.util.TreeSet;
 
+/**
+ * The controller for the ResourcesPanel
+ */
 public class ResourceController {
+    /**
+     * THe database connection
+     */
     private Database database;
+
+    /**
+     * The ResourcesPanel
+     */
     private ResourcesPanel rp;
 
+    /**
+     * Creates a new ResourceController
+     * @param database The Database
+     */
     public ResourceController(Database database) {
         this.database = database;
         rp = new ResourcesPanel(this);
         update();
     }
 
+    /**
+     * Deletes a Resource form the database
+     * @param r the Resource to delete
+     */
     public void delete(Resource r) {
         try {
             r.delete();
@@ -30,6 +48,10 @@ public class ResourceController {
         update();
     }
 
+    /**
+     * Adds a new Resource to the database
+     * @param newResourceText the name of the Resource
+     */
     public void add(String newResourceText) {
         try {
             new Resource(rp.getNewResourceText(), database);
@@ -45,6 +67,9 @@ public class ResourceController {
         rp.clearNewResourceText();
     }
 
+    /**
+     * Updates the ResourcesPanel
+     */
     public void update() {
         try {
             TreeSet<Resource> allResources = Resource.getAllResources(database);
@@ -56,6 +81,10 @@ public class ResourceController {
         System.out.println("update");
     }
 
+    /**
+     * Gets the ResourcesPanel
+     * @return the ResourcesPanel
+     */
     public ResourcesPanel getRp() {
         return rp;
     }
